cd11b06773b9a6d83c8283e1a6ad06895ccf8f46
spinooooo <clendrickarellano@gmail.com>
2025-03-12 17:50:47 +0800

    Finished CheckingAccount SubClass

--- src/models/CheckingAccount.java
+++ src/models/CheckingAccount.java
@@ -1,4 +1,35 @@
 package models;
 
-public class CheckingAccount {
+//let me know if there are any issues
+//or new additions you would like to see/ are needed :>
+
+public class CheckingAccount extends BankAccounts {
+    public double minimumBalance;
+
+    public CheckingAccount () {
+        this.minimumBalance = 0;
+    }
+
+    public CheckingAccount (int accountNo, String accountName, double minimumBalance) {
+        super(accountNo, accountName, 0.0, "active");
+        this.minimumBalance = minimumBalance;
+    }
+
+    public void setMinimumBalance(double minimumBalance) {
+        this.minimumBalance = minimumBalance;
+    }
+
+    public double getMinimumBalance() {
+        return minimumBalance;
+    }
+
+    public void encashCheck (double amount) {
+        if (amount >= minimumBalance) {
+            System.out.println("Encashing of " + amount + " pesos is successful");
+            withdraw(amount);
+        } else  {
+            System.out.println("Your withdrawal exceeds the minimum balance");
+        }
+
+    }
 }
