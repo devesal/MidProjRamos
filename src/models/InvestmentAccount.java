1a2eab53b3a0a7aa215fc0f9ee531781896b858f
Teo <theooaquino@gmail.com>
2025-03-13 10:59:47 +0800

    Changed some methods and disabled money transfers and withdrawal with an override method.

--- src/models/InvestmentAccount.java
+++ src/models/InvestmentAccount.java
@@ -5,13 +5,14 @@ public class InvestmentAccount extends BankAccount {
     private double minimumBalance;
     private double interest;
 
-    public InvestmentAccount(){
+    public InvestmentAccount(int accountNo, String accountName, String status){
+        super(accountNo, accountName, status);
         minimumBalance = 0.0;
         interest = 0.0;
+        status = "Active";
     }
 
-    public InvestmentAccount(int accountNo, String accountName, double minimumBalance, double interest){
-        super(accountNo, accountName, 0.0, "Active"); // Not sure why, but this line has an error in my IDE
+    public InvestmentAccount(double minimumBalance, double interest){
         this.minimumBalance = minimumBalance;
         this.interest = interest;
     }
@@ -33,14 +34,26 @@ public class InvestmentAccount extends BankAccount {
         System.out.println("Your investment value is: " + investmentValue);
     }
 
+    public void transferMoney(int accountNo, double amount, BankAccount[]listOfBankAccounts){
+        System.out.println("You cannot transfer money from an investment account.");
+    }
+
+    public void withdraw(double amount){
+        System.out.println("You cannot withdraw money from an investment account.");
+        System.out.println("Please close your account to withdraw your investment.");
+    }
+
     public void closeAccount() {
         double finalBalance = super.inquireBalance() * (1 + interest);
         if (finalBalance > 0) {
             withdraw(finalBalance);
-            super.closeAccount();
+            super.setStatus("Closed");
+            System.out.println("Your account has been closed.");
 
         } else {
             System.out.println("There is no balance to withdraw.");
+            super.setStatus("Closed");
+            System.out.println("Your account has been closed.");
 
         }
 
