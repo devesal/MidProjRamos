36bf3ffc388831340b474af56ce9683153ddaf58
spinooooo <clendrickarellano@gmail.com>
2025-03-13 12:30:21 +0800

    Added an @Override with the withdraw method in this class to not allow withdrawals with a checking account but instead the user must encash the check instead.

--- src/models/CheckingAccount.java
+++ src/models/CheckingAccount.java
@@ -20,11 +20,16 @@ public class CheckingAccount extends BankAccount {
         return minimumBalance;
     }
 
+    @Override
+    public void withdraw(double amount) {
+        System.out.println("Withdrawal is not allowed with a checking account");
+        System.out.println("Please use the encash check option instead.");
+    }
 
     public void encashCheck (double amount) {
         if (amount >= minimumBalance) {
             System.out.println("Encashing of " + amount + " pesos is successful");
-            withdraw(amount);
+            super.withdraw(amount);
         } else  {
             System.out.println("Your withdrawal exceeds the minimum balance");
         }
